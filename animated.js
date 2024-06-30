180d257b3ca0e0d7c21ee4dd0c8aa7ac88e1e45b
noumanmani <168138758+noumanmani@users.noreply.github.com>
2024-06-30 20:16:29 +0500

    haif complete

--- animated.js
+++ animated.js
@@ -1,13 +1,25 @@
 
-function hide(){
-    let nono=document.querySelector(".btn");
-    nono.style.backgroundClore="blue" 
+// function hide(){
+//     let nono=document.querySelector(".btn");
+//     nono.style.backgroundClore="blue" 
 
-}
-function hide(){
-    const butt =document.querySelector(".btn")
-    butt.style.backgroundColor="Blue" 
-}
-function fixd(){
+// }
+// function hide(){
+//     const butt =document.querySelector(".btn")
+//     butt.style.backgroundColor="Blue" 
+// }
+// function fixd(){
     
-}
+// }
+
+// function show(){
+//     const side =document.querySelector(".side-bar")
+//     side.style.display="flex" 
+//  }
+// function hide(){
+//     const side =document.querySelector(".side-bar")
+//     side.style.display="none" 
+// }
+//  let nou =()=>{
+    
+//  }
