bb79ed5c534346fea36eb15a5dc7def2e6e89b86
noumanmani <168138758+noumanmani@users.noreply.github.com>
2024-07-04 03:33:58 +0500

    complete

--- animated.js
+++ animated.js
@@ -1,25 +1,28 @@
 
-// function hide(){
-//     let nono=document.querySelector(".btn");
-//     nono.style.backgroundClore="blue" 
+function hide(){
+    let nono=document.querySelector(".btn");
+    nono.style.backgroundClore="blue" 
 
-// }
-// function hide(){
-//     const butt =document.querySelector(".btn")
-//     butt.style.backgroundColor="Blue" 
-// }
-// function fixd(){
+}
+function hide(){
+    const butt =document.querySelector(".btn")
+    butt.style.backgroundColor="Blue" 
+}
+function fixd(){
     
-// }
+}
 
-// function show(){
-//     const side =document.querySelector(".side-bar")
-//     side.style.display="flex" 
-//  }
-// function hide(){
-//     const side =document.querySelector(".side-bar")
-//     side.style.display="none" 
-// }
-//  let nou =()=>{
+function show(){
+    const side =document.querySelector(".side-bar")
+    side.style.display="flex" 
+ }
+function hide(){
+    const side =document.querySelector(".side-bar")
+    side.style.display="none" 
+}
+function open(){
+    let open=document.querySelector('.open');
+    let nave=document.querySelector('.margin-lift')
     
-//  }
+
+}
